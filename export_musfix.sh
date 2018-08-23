7eb9f84f37a9187e82d8f03936c406e1d45d1c3b
Kevin <khine@ucsd.edu>
2018-08-23 14:33:14 -0700

    Updated export script

--- export_musfix.sh
+++ export_musfix.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for file in "$@"; do
+search_dir="tests/**/*.fq"
+
+stack build && 
+for file in ${search_dir}; do
 	stack exec -- fixpoint ${file}
-	filename=$(basename ${file} .fq)
-	mv lqfp2musfix.smt2 ../musfix/test/liquid/${filename}.smt2 
+	extensionless=${file%.fq}
+	filename=${extensionless#*/} 
+	mv lqfp2musfix.smt2 ../musfix/test/${filename}.smt2_
 done
